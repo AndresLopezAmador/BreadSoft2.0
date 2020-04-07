ee4beac31415d8f7fab53105a1d890bcd01a8496
Enrique de jesus Zepeda Ontiveros <quique-43@hotmail.com>
2020-04-07 14:02:37 -0500

    Se agregaron los modelos Pedido y Producto

---
+++ BreadSoft/BreadSoft/Models/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BreadSoft.Models
+{
+    public class Pedido
+    {
+        public int idProducto;
+        public string nombre;
+        public string descripcion;
+        public int cantidad;
+        public double Precio;
+        public string foto;
+    }
+}
---
+++ BreadSoft/BreadSoft/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BreadSoft.Models
+{
+    public class Producto
+    {
+        public int idPedido;
+        public int cantidad;
+        public double pago;
+        public DateTime fechaPedido;
+        public DateTime fechaEntrega;
+        public int idProducto;
+        public int idEmpleado;
+        public int idCliente;
+        public int idProveedor;
+    }
+}
