4378b50dcb18464e58bc3eab99981c48272a486c
Sergio Francisco Torres Vazquez <17001747@alumnos.utleon.edu.mx>
2020-04-06 20:59:15 -0500

    Me rife el modulo de persona de nada puercos :D

---
+++ BreadSoft/BreadSoft/Models/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BreadSoft.Models
+{
+    public class Persona
+    {
+        public int Id { set; get; }
+        public string Nombre { get; set;}
+}
