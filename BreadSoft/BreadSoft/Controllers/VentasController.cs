756f094ae677be784496c9fee21c08868f8c54b8
Sergio Francisco Torres Vazquez <17001747@alumnos.utleon.edu.mx>
2020-04-08 18:22:52 -0500

    Creacion de controller <Ventas>

---
+++ BreadSoft/BreadSoft/Controllers/VentasController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BreadSoft.Controllers
+{
+    public class VentasController : Controller
+    {
+        // GET: Ventas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+
+        public ActionResult Modificar()
+        {
+            return View();
+        }
+
+        public ActionResult MostrarTodo()
+        {
+            return View();
+        }
+
+
+    }
+}
