ecc6e56357f1f3df7f7f6766bf140c2789cab95c
Sergio Francisco Torres Vazquez <17001747@alumnos.utleon.edu.mx>
2020-04-13 20:44:23 -0500

    correcion linea entities

--- BreadSoft/BreadSoft/Controllers/EmpleadosController.cs
+++ BreadSoft/BreadSoft/Controllers/EmpleadosController.cs
@@ -14,7 +14,7 @@ namespace BreadSoft.Controllers
         // GET: Empleados
         public ActionResult Index()
         {
-            var db = new BreadSoftv2EntitiesEnr();
+            var db = new BreadSoftv2Entities();
             var data = db.empleado.ToList();
             return View(data);
         }
